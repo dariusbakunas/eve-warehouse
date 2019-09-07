3497c905baa6328029fe8580dc03db3eb800f64a
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-09-07 19:03:41 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
+}
+
+export default App;
