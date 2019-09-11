39e74254a22dd5688cf8d9d8be870fdfe1b558cb
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-09-11 19:08:57 -0400

    few more storybook addons

--- stories/Header.stories.tsx
+++ stories/Header.stories.tsx
@@ -4,6 +4,6 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import Header from "../src/components/Header";
 
-storiesOf("Header", module)
+storiesOf("Components|Header", module)
   .add("Not authenticated", () => <Header isAuthenticated={false} onLoginClick={action("login click")} title="Header" />)
   .add("Authenticated", () => <Header isAuthenticated={true} onLogoutClick={action("logout click")} title="Header" />);
