64a755e819980978cd338c33a42c071654c0155b
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-09-11 20:47:02 -0400

    add mui theme support to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,11 @@
 import { configure } from "@storybook/react";
 import { addDecorator } from "@storybook/react";
 import { withA11y } from "@storybook/addon-a11y";
+import { muiTheme } from "storybook-addon-material-ui";
+import theme from "../src/theme";
 
 addDecorator(withA11y);
+addDecorator(muiTheme(theme));
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../stories", true, /\.stories\.tsx$/);
