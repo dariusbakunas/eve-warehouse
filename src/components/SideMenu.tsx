b4f9f8dd8016945cc971e996f652aa453e9664c3
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-09-21 09:14:21 -0400

    add side menu story

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,61 +1,61 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx'
-import IconButton from '@material-ui/core/IconButton'
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import Divider from '@material-ui/core/Divider'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
-import Drawer from '@material-ui/core/Drawer'
-import DashboardIcon from '@material-ui/icons/Dashboard';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import IconButton from "@material-ui/core/IconButton";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Drawer from "@material-ui/core/Drawer";
+import DashboardIcon from "@material-ui/icons/Dashboard";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
+    position: "relative",
+    whiteSpace: "nowrap",
     width: drawerWidth,
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
   drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
+      duration: theme.transitions.duration.leavingScreen
     }),
     width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9)
+    }
   },
   toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  }
 }));
 
 interface ISideMenuProps {
-  onSideMenuClose: () => void,
-  sideMenuOpen: boolean,
+  onSideMenuClose: () => void;
+  sideMenuOpen: boolean;
 }
 
 export const SideMenu: React.FC<ISideMenuProps> = ({ onSideMenuClose, sideMenuOpen }) => {
-  const classes = useStyles();
+  const classes = useStyles({});
 
   return (
     <Drawer
-      variant="permanent"
+      variant="persistent"
       classes={{
-        paper: clsx(classes.drawerPaper, !sideMenuOpen && classes.drawerPaperClose),
+        paper: clsx(classes.drawerPaper, !sideMenuOpen && classes.drawerPaperClose)
       }}
       open={sideMenuOpen}
     >
@@ -66,14 +66,12 @@ export const SideMenu: React.FC<ISideMenuProps> = ({ onSideMenuClose, sideMenuOp
       </div>
       <Divider />
       <List>
-        <div>
-          <ListItem button>
-            <ListItemIcon>
-              <DashboardIcon />
-            </ListItemIcon>
-            <ListItemText primary="Dashboard" />
-          </ListItem>
-        </div>
+        <ListItem button>
+          <ListItemIcon>
+            <DashboardIcon />
+          </ListItemIcon>
+          <ListItemText primary="Dashboard" />
+        </ListItem>
       </List>
     </Drawer>
   );
---
+++ stories/SideMenu.stories.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import Header from "../src/components/Header";
+import SideMenu from "../src/components/SideMenu";
+
+const MenuExample: React.FC = () => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <div style={{ display: "flex", flexGrow: 1, height: "100vh" }}>
+      <Header isAuthenticated={true} sideMenuOpen={open} onSideMenuOpen={() => setOpen(true)} sideMenuEnabled={true} onLogoutClick={action("logout click")} title="Header" />
+      <SideMenu sideMenuOpen={open} onSideMenuClose={() => setOpen(false)} />
+    </div>
+  );
+};
+
+storiesOf("Components|SideMenu", module).add("Default", () => <MenuExample />);
