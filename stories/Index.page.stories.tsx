746436f443338b135d8eaa92bb9633e22da6ef44
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-09-21 09:14:30 -0400

    monir story updates

--- stories/Index.page.stories.tsx
+++ stories/Index.page.stories.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
 import Index from "../src/pages/Index";
 
-storiesOf("Pages|Index", module)
-  .add("Default", () => <Index />);
+storiesOf("Pages|Index", module).add("Default", () => <Index />);
