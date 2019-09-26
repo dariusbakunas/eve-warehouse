bd39e21c406457d123162863c651190f64ab846a
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-09-25 20:04:53 -0400

    add gql mock to storyboard

--- .storybook/config.jsx
+++ .storybook/config.jsx
@@ -1,13 +1,22 @@
 import { configure } from "@storybook/react";
 import { addDecorator } from "@storybook/react";
+import apolloStorybookDecorator from "apollo-storybook-react";
 import { withA11y } from "@storybook/addon-a11y";
 import Root from "../src/components/Root";
 import React from 'react'
+import { mocks, typeDefs } from "./graphqlMocks"
 
 addDecorator(withA11y);
 
 const RootDecorator = storyFn => <Root>{storyFn()}</Root>;
 addDecorator(RootDecorator);
+
+addDecorator(
+  apolloStorybookDecorator({
+    typeDefs,
+    mocks
+  })
+);
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../stories", true, /\.stories\.tsx$/);
