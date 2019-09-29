fc474b82593c561aa409649c28475dcc3be65bbc
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-09-29 09:37:46 -0400

    add router decorator

---
+++ .storybook/config.tsx
@@ -0,0 +1,52 @@
+import { configure } from "@storybook/react";
+import { addDecorator } from "@storybook/react";
+import { NextRouter } from 'next/router';
+import { RouterContext } from 'next-server/dist/lib/router-context';
+import apolloStorybookDecorator from "apollo-storybook-react";
+import { withA11y } from "@storybook/addon-a11y";
+import Root from "../src/components/Root";
+import React, { ReactNode } from 'react'
+import { mocks, typeDefs } from "./graphqlMocks"
+import { StoryFn } from '@storybook/addons/src/types'
+
+addDecorator(withA11y);
+
+const RootDecorator = (storyFn: StoryFn<ReactNode>) => <Root>{storyFn()}</Root>;
+addDecorator(RootDecorator);
+
+const router: NextRouter = {
+  asPath: '/',
+  events: {
+    emit: () => {},
+    on: () => {},
+    off: () => {},
+  },
+  pathname: '/',
+  query: {},
+  back: () => Promise.resolve(true),
+  beforePopState: () => Promise.resolve(true),
+  prefetch: () => Promise.resolve(),
+  push: () => Promise.resolve(true),
+  reload: () => Promise.resolve(true),
+  replace: () => Promise.resolve(true),
+  route: '/',
+};
+
+const RouterDecorator = (storyFn: StoryFn<ReactNode>) => <RouterContext.Provider value={router}>{storyFn()}</RouterContext.Provider>;
+addDecorator(RouterDecorator);
+
+addDecorator(
+  apolloStorybookDecorator({
+    typeDefs,
+    mocks
+  })
+);
+
+// automatically import all files ending in *.stories.js
+// @ts-ignore
+const req = require.context("../stories", true, /\.stories\.tsx$/);
+function loadStories() {
+  req.keys().forEach((filename: string) => req(filename));
+}
+
+configure(loadStories, module);
--- stories/SideMenu.stories.tsx
+++ stories/SideMenu.stories.tsx
@@ -1,48 +1,32 @@
-import React from "react";
+import React from 'react';
 
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import Header from "../src/components/Header";
-import { SideMenu } from "../src/components/SideMenu";
-import { NextRouter } from "next/router";
-import { RouterContext } from "next-server/dist/lib/router-context";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import DashboardIcon from "@material-ui/icons/Dashboard";
-import CharactersIcon from "../src/icons/CharactersIcon";
-import SideMenuHeader from "../src/components/SideMenuHeader";
-
-const router: NextRouter = {
-  asPath: "/",
-  events: {
-    emit: () => {},
-    on: () => {},
-    off: () => {}
-  },
-  pathname: "/",
-  query: {},
-  back: () => Promise.resolve(true),
-  beforePopState: () => Promise.resolve(true),
-  prefetch: () => Promise.resolve(),
-  push: () => Promise.resolve(true),
-  reload: () => Promise.resolve(true),
-  replace: () => Promise.resolve(true),
-  route: "/"
-};
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Header from '../src/components/Header';
+import { SideMenu } from '../src/components/SideMenu';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import CharactersIcon from '../src/icons/CharactersIcon';
+import SideMenuHeader from '../src/components/SideMenuHeader';
 
 const MenuExample: React.FC = () => {
   return (
-    <RouterContext.Provider value={router}>
-      <Header isAuthenticated={true} onLogoutClick={action("logout click")} title="Header" />
+    <React.Fragment>
+      <Header
+        isAuthenticated={true}
+        onLogoutClick={action('logout click')}
+        title="Header"
+      />
       <SideMenu
         header={() => (
           <SideMenuHeader
             user={{
-              name: "Test User",
-              email: "test@gmail.com",
-              picture: "https://avatars0.githubusercontent.com/u/2111392?v=4"
+              name: 'Test User',
+              email: 'test@gmail.com',
+              picture: 'https://avatars0.githubusercontent.com/u/2111392?v=4',
             }}
           />
         )}
@@ -52,18 +36,24 @@ const MenuExample: React.FC = () => {
             <ListItemIcon>
               <DashboardIcon />
             </ListItemIcon>
-            <ListItemText primary="Dashboard" primaryTypographyProps={{ noWrap: true }} />
+            <ListItemText
+              primary="Dashboard"
+              primaryTypographyProps={{ noWrap: true }}
+            />
           </ListItem>
           <ListItem selected={false} button>
             <ListItemIcon>
               <CharactersIcon />
             </ListItemIcon>
-            <ListItemText primary="Characters" primaryTypographyProps={{ noWrap: true }} />
+            <ListItemText
+              primary="Characters"
+              primaryTypographyProps={{ noWrap: true }}
+            />
           </ListItem>
         </List>
       </SideMenu>
-    </RouterContext.Provider>
+    </React.Fragment>
   );
 };
 
-storiesOf("Components|SideMenu", module).add("Default", () => <MenuExample />);
+storiesOf('Components|SideMenu', module).add('Default', () => <MenuExample />);
