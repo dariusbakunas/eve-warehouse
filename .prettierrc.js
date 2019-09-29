3f16b6fa54e64139d8545b5e4efa29ef0dc2341f
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-09-29 09:20:16 -0400

    fix prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,12 @@
-const config = {
+module.exports = {
   bracketSpacing: true,
   htmlWhitespaceSensitivity: "css",
   jsxBracketSameLine: false,
   jsxSingleQuote: false,
   quoteProps: "as-needed",
   semi: true,
-  trailingComma: "all",
+  trailingComma: "es5",
   singleQuote: true,
-  printWidth: 150,
-  tabWidth: 4
+  printWidth: 80,
+  tabWidth: 2
 };
-
-export default config;
