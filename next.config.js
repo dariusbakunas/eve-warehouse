dd326861c4d66c41ac04da4e5bc7e776f19b23dc
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-09-29 09:24:05 -0400

    add client env vars

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
+const { parsed: localEnv } = require("dotenv").config();
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const webpack = require("webpack");
+
+const CLIENT_ENV_WHITELIST = ["EVE_API_HOST", "EVE_LOGIN_URL", "EVE_CLIENT_ID", "EVE_CHARACTER_REDIRECT_URL"];
+
+const env = CLIENT_ENV_WHITELIST.reduce((acc, key) => {
+  const val = localEnv[key];
+  if (val) {
+    acc[key] = val;
+  }
+
+  return acc;
+}, {});
+
 module.exports = {
-  webpack: (config, options) => {
+  webpack: config => {
     config.module.rules.push({
       test: /\.(graphql|gql)$/,
       exclude: /node_modules/,
       loader: "graphql-tag/loader"
     });
+
+    config.plugins.push(new webpack.EnvironmentPlugin(env));
 
     return config;
   }
