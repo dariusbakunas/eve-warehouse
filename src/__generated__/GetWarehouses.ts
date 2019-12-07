e4decccee1a374aeb217e5d75f2357816818792a
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-12-07 12:01:39 -0500

    warehouse queries

---
+++ src/__generated__/GetWarehouses.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: GetWarehouses
+// ====================================================
+
+export interface GetWarehouses_warehouses {
+  __typename: "Warehouse";
+  id: string;
+  name: string;
+}
+
+export interface GetWarehouses {
+  warehouses: GetWarehouses_warehouses[];
+}
---
+++ src/__generated__/RemoveWarehouse.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: RemoveWarehouse
+// ====================================================
+
+export interface RemoveWarehouse {
+  removeWarehouse: string;
+}
+
+export interface RemoveWarehouseVariables {
+  id: string;
+}
