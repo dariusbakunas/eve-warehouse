80536352679b07cfe2e3042b0e34c30ef74450fe
Darius Bakunas-Milanowski <bakunas@gmail.com>
2019-12-18 22:47:47 -0500

    remove ugly skill progress for now

--- src/components/SkillGroupButton.tsx
+++ src/components/SkillGroupButton.tsx
@@ -1,11 +1,11 @@
-import React, { useCallback } from 'react';
-import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
-import ButtonBase from '@material-ui/core/ButtonBase';
-import Typography from '@material-ui/core/Typography';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { GetCharacterSkillGroups_character_skillGroups as SkillGroup } from '../__generated__/GetCharacterSkillGroups';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import NeuralEnchancementIcon from '../icons/NeuralEnhancementIcon';
+import React, { useCallback } from 'react';
 import ShipsIcon from '../icons/ShipsIcon';
 import SkillsIcon from '../icons/SkillsIcon';
-import NeuralEnchancementIcon from '../icons/NeuralEnhancementIcon';
+import Typography from '@material-ui/core/Typography';
 
 interface ISkillGroupButtonProps {
   selected?: boolean;
@@ -16,35 +16,17 @@ interface ISkillGroupButtonProps {
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     label: {
-      color: 'white',
       position: 'absolute',
       top: 0,
       left: 5,
     },
     root: {
-      '&:hover': {
-        backgroundColor: 'rgb(214,223,227)',
-      },
       position: 'absolute',
       left: 30,
       top: 0,
       height: '25px',
       overflow: 'hidden',
       minWidth: '220px',
-      backgroundColor: ({ selected }: Partial<ISkillGroupButtonProps>) => {
-        return selected ? 'rgb(184,193,197)' : theme.palette.primary.light;
-      },
-    },
-    progress: {
-      transition: 'transform .4s linear',
-      backgroundColor: ({ selected }: Partial<ISkillGroupButtonProps>) => {
-        return selected ? theme.palette.primary.dark : theme.palette.primary.main;
-      },
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      bottom: 0,
-      width: '100%',
     },
   })
 );
@@ -79,11 +61,8 @@ const SkillGroupButton: React.FC<ISkillGroupButtonProps> = ({ group, onClick, se
   return (
     <ButtonBase onClick={handleClick} disableRipple={false}>
       <div style={{ position: 'relative', height: '25px', width: '250px' }}>
-        <span style={{ position: 'absolute', left: 0 }}>
-          {getSkillGroupIcon(group.name)}
-        </span>
+        <span style={{ position: 'absolute', left: 0 }}>{getSkillGroupIcon(group.name)}</span>
         <div className={classes.root}>
-          <div className={classes.progress} style={{ transform: `translateX(${completed - 100}%)` }} />
           <Typography component="span" variant="subtitle1" color="inherit" className={classes.label}>
             {group.name}
           </Typography>
