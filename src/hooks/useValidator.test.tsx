5427514e2fedb0ad319f92900decbeb7af3e217f
Darius Bakunas-Milanowski <bakunas@gmail.com>
2020-01-24 21:19:29 -0500

    more validation hook tests

--- src/hooks/useValidator.test.tsx
+++ src/hooks/useValidator.test.tsx
@@ -6,35 +6,87 @@ import useValidator from './useValidator';
 
 interface IFormData {
   numProp: Maybe<number>;
+  emailProp: Maybe<string>;
+  passwordProp: Maybe<string>;
 }
 
 const TestForm: React.FC<{ onSubmit?: (data: IFormData) => void }> = ({ onSubmit }) => {
   const { register, handleSubmit, errors, setValue, values } = useValidator<IFormData>();
 
   useEffect(() => {
-    register('numProp', { required: true });
+    register('numProp', { required: true, min: 5, max: 10 });
+    register('emailProp', { email: true });
+    register('passwordProp', { password: true });
   }, []);
 
   return (
     <div>
       <input id="numInput" type="number" onChange={event => setValue('numProp', event.target.value ? +event.target.value : null)} />
-      <span id="error">{errors.numProp ? errors.numProp.message : ''}</span>
+      <input id="emailInput" onChange={event => setValue('emailProp', event.target.value ? event.target.value : null)} />
+      <input id="passwordInput" onChange={event => setValue('passwordProp', event.target.value ? event.target.value : null)} />
+      <span id="numError">{errors.numProp ? errors.numProp.message : ''}</span>
+      <span id="emailError">{errors.emailProp ? errors.emailProp.message : ''}</span>
+      <span id="passwordError">{errors.passwordProp ? errors.passwordProp.message : ''}</span>
       <button onClick={handleSubmit(onSubmit)}>submit</button>
     </div>
   );
 };
 
 describe('useValidator', () => {
-  beforeEach(() => {
-    jest.spyOn(React, 'useEffect');
-  });
-
-  it('sets error for empty required field', () => {
+  it('validates required field', () => {
     const useEffectSpy = jest.spyOn(React, 'useEffect');
     const submitMock = jest.fn();
     useEffectSpy.mockImplementationOnce(f => f());
     const wrapper = shallow(<TestForm onSubmit={submitMock} />);
-    wrapper.find('input').simulate('change', { target: { value: '' } });
-    expect(wrapper.find('#error').text()).toEqual('This field is required');
+    wrapper.find('#numInput').simulate('change', { target: { value: '' } });
+    expect(wrapper.find('#numError').text()).toEqual('This field is required');
+  });
+
+  it('validates min field', () => {
+    const useEffectSpy = jest.spyOn(React, 'useEffect');
+    const submitMock = jest.fn();
+    useEffectSpy.mockImplementationOnce(f => f());
+    const wrapper = shallow(<TestForm onSubmit={submitMock} />);
+    wrapper.find('#numInput').simulate('change', { target: { value: 4 } });
+    expect(wrapper.find('#numError').text()).toEqual('Must be at least 5');
+  });
+
+  it('validates max field', () => {
+    const useEffectSpy = jest.spyOn(React, 'useEffect');
+    const submitMock = jest.fn();
+    useEffectSpy.mockImplementationOnce(f => f());
+    const wrapper = shallow(<TestForm onSubmit={submitMock} />);
+    wrapper.find('#numInput').simulate('change', { target: { value: 12 } });
+    expect(wrapper.find('#numError').text()).toEqual('Must not exceed 10');
+  });
+
+  it('validates email field', () => {
+    const useEffectSpy = jest.spyOn(React, 'useEffect');
+    const submitMock = jest.fn();
+    useEffectSpy.mockImplementationOnce(f => f());
+    const wrapper = shallow(<TestForm onSubmit={submitMock} />);
+    wrapper.find('#emailInput').simulate('change', { target: { value: 'test' } });
+    expect(wrapper.find('#emailError').text()).toEqual('Invalid email');
+  });
+
+  it('validates password field', () => {
+    const useEffectSpy = jest.spyOn(React, 'useEffect');
+    const submitMock = jest.fn();
+    useEffectSpy.mockImplementationOnce(f => f());
+    const wrapper = shallow(<TestForm onSubmit={submitMock} />);
+    wrapper.find('#passwordInput').simulate('change', { target: { value: 'simpleP@ssword' } });
+    expect(wrapper.find('#passwordError').text()).toEqual('The password must contain at least one number.');
+  });
+
+  it('resets error once validation passes', () => {
+    const useEffectSpy = jest.spyOn(React, 'useEffect');
+    const submitMock = jest.fn();
+    useEffectSpy.mockImplementationOnce(f => f());
+    useEffectSpy.mockImplementationOnce(f => f());
+    const wrapper = shallow(<TestForm onSubmit={submitMock} />);
+    wrapper.find('#numInput').simulate('change', { target: { value: 4 } });
+    expect(wrapper.find('#numError').text()).toEqual('Must be at least 5');
+    wrapper.find('#numInput').simulate('change', { target: { value: 6 } });
+    expect(wrapper.find('#numError').text()).toEqual('');
   });
 });
