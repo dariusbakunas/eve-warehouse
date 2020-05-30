2477396efc75528f00e5d584ee931e499f9d3ff6
Darius Bakunas-Milanowski <bakunas@gmail.com>
2020-05-30 12:01:03 -0400

    add character update query

--- src/pages/Characters/Characters.tsx
+++ src/pages/Characters/Characters.tsx
@@ -4,19 +4,37 @@ import { CharacterTile } from "../../components/CharacterTile/CharacterTile";
 import { loader } from "graphql.macro";
 import { Loading } from "carbon-components-react";
 import { Maybe } from "../../utilityTypes";
+import { RootState } from "../../redux/reducers";
+import { UpdateCharacter, UpdateCharacterVariables } from "../../__generated__/UpdateCharacter";
+import { useHistory, useLocation } from "react-router-dom";
+import { useMutation, useQuery } from "@apollo/react-hooks";
 import { useNotification } from "../../components/Notifications/useNotifications";
-import { useQuery } from "@apollo/react-hooks";
+import { useSelector } from "react-redux";
 import _ from "lodash";
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 
 const getCharactersQuery = loader("../../queries/getCharacters.graphql");
+const updateCharacterMutation = loader("../../queries/updateCharacter.graphql");
 
 export const Characters: React.FC = () => {
+  const appConfig = useSelector<RootState, RootState["appConfig"]>((state) => state.appConfig);
   const [updateScopesModalVisible, setUpdateScopesModalVisible] = useState(false);
   const [currentCharacter, setCurrentCharacter] = useState<Maybe<Character>>(null);
   const { enqueueNotification } = useNotification();
+  const history = useHistory();
+  const location = useLocation();
 
   const { loading: charactersLoading, data, error } = useQuery<CharactersResponse>(getCharactersQuery);
+
+  const [updateCharacter, { loading: characterUpdateLoading }] = useMutation<UpdateCharacter, UpdateCharacterVariables>(updateCharacterMutation, {
+    onError: (error) => {
+      enqueueNotification(`Character update failed: ${error.message}`, null, { kind: "error" });
+    },
+    onCompleted: (data) => {
+      enqueueNotification(`Character '${data.updateCharacter.name}' updated successfully`);
+      history.push("/characters");
+    },
+  });
 
   const rows = useMemo(() => {
     if (data) {
@@ -53,13 +71,42 @@ export const Characters: React.FC = () => {
 
       if (scopes && scopes.length) {
         const state = btoa(JSON.stringify({ id: character.id }));
-
+        window.location.href = `${appConfig.eveLoginUrl}/oauth/authorize?response_type=code&redirect_uri=${
+          appConfig.eveCharacterRedirectUrl
+        }&client_id=${appConfig.eveClientId}&scope=${scopes.join(" ")}&state=${state}`;
       } else {
         console.warn(`skipping ${character.name} update, no scopes were selected`);
       }
     },
     [currentCharacter]
   );
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const code = params.get("code");
+    const state = params.get("state");
+
+    if (code) {
+      if (state) {
+        // character update
+        try {
+          const stateJson = atob(state);
+          const { id } = JSON.parse(stateJson);
+          updateCharacter({
+            variables: {
+              id: id,
+              code,
+            },
+          });
+        } catch (e) {
+          enqueueNotification(`Character update failed: ${e.message}`, null, { kind: "error" });
+          console.error(e);
+        }
+      } else {
+        // new character
+      }
+    }
+  }, [location]);
 
   return (
     <div className="characters">
