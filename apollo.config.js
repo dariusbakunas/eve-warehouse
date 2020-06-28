79bd7ab4b742b22199639f8a9e0d5c1e99204eb2
Darius Bakunas-Milanowski <bakunas@gmail.com>
2020-06-28 11:42:03 -0400

    generate types during build

--- apollo.config.js
+++ apollo.config.js
@@ -2,10 +2,10 @@ module.exports = {
   client: {
     service: {
       addTypename: true,
-      name: "eve-warehouse",
-      includes: ["src/queries/*.graphql"],
-      localSchemaFile: "src/__generated__/schema.graphql",
+      name: 'eve-warehouse',
+      includes: ['src/queries/*.graphql'],
+      localSchemaFile: 'src/__generated__/schema.graphql',
     },
-    excludes: ["src/__generated__/schema.graphql"],
+    excludes: ['src/__generated__/schema.graphql'],
   },
 };
