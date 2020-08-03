59cdda529831271f780610026bbb4735f06e76c0
Darius Bakunas-Milanowski <bakunas@gmail.com>
2020-08-02 22:56:39 -0400

    separate server/client dependencies

---
+++ client/src/api/apiError.ts
@@ -0,0 +1,11 @@
+export class ApiError extends Error {
+  public url?: string;
+  public body?: string;
+  public status?: string;
+  public statusText?: string;
+
+  constructor(message: string) {
+    super(message);
+    this.name = 'ApiError';
+  }
+}
